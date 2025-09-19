8a9b8720f92956d38d07d13e8afce5c589ff842e
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2025-09-19 16:00:21 +0600

    feat: add Countries component and update App structure

--- react-pro/src/App.jsx
+++ react-pro/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
   return (
     <>
       
-      <h1>React</h1>
+      <Countries></Countries>
       
     </>
   )
---
+++ react-pro/src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+             <h1>  Name : S M Samiul Hasan </h1>
+        </div>
+    );
+}; 
+
+export default Countries;
